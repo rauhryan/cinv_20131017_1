f1c986f7046e7bc646efc88a5be2f5c8c98cf229
JB Smith <jbsmith@me.com>
2014-01-13 15:02:16 -0600

    Pushing new changes

--- app/assets/javascripts/movies/movie_library.js
+++ app/assets/javascripts/movies/movie_library.js
@@ -1,4 +1,42 @@
 (function(targetNamespace){
 
+    targetNamespace.MovieLibrary = function MovieLibrary(list){
+        var listOfMovies = list;
+        var response = {
+            allPixarMovies: function(){
+                var result = [];
+                for(var i = 0; i < listOfMovies.length; i++){
+                    if(listOfMovies[i].studio===STUDIOS.PIXAR){
+                        result.push(listOfMovies[i]);
+                    }
+                }
+                return result;
+            }
+            ,allPixarOrDisneyMovies: function(){
+                var result = [];
+                for(var i = 0; i < listOfMovies.length; i++){
+                    if(listOfMovies[i].studio===STUDIOS.PIXAR || listOfMovies[i].studio===STUDIOS.DISNEY){
+                        result.push(listOfMovies[i]);
+                    }
+                }
+                return result;
+            }
+            ,allMoviesNotPublishedByPixar: function(){
+
+            }
+            ,allMoviesReleasedAfter: function(){
+
+            }
+            ,allMoviesReleasedBetweenYears: function(){
+
+            }
+            ,sortAllMoviesByDescendingTitle: function(){
+
+            }
+
+        };
+        return response;
+    }
+
 })(this);
 
